af33dc5747328963cf3ecf50bf0b0fd8c13ff639
meghu <meghuraval1234@yahoo.com>
2023-12-27 20:37:16 -0500

    added components

---
+++ client/components/Navbar.jsx
@@ -0,0 +1,6 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+
+export default function Navbar() {
+  return <div>Navbar</div>;
+}
---
+++ client/components/index.js
@@ -0,0 +1,22 @@
+import BookRow from "./BookRow";
+import FeaturedProduct from "./FeaturedProduct";
+import Footer from "./Footer";
+import GenreTitle from "./GenreTitle";
+import GenreTitleTwo from "./GenreTitleTwo";
+import Navbar from "./Navbar";
+
+BookRow;
+FeaturedProduct;
+Footer;
+GenreTitle;
+GenreTitleTwo;
+Navbar;
+
+export default {
+  BookRow,
+  FeaturedProduct,
+  Footer,
+  GenreTitle,
+  GenreTitleTwo,
+  Navbar,
+};

6f6aeb418db4c2a3329187cf47b2acc795b89bf8
meghu <meghuraval1234@yahoo.com>
2024-01-02 03:21:37 -0500

    signout and update functionality added and also added functionality to update navbar based on authentication status

--- client/src/pages/AccountPage.jsx
+++ client/src/pages/AccountPage.jsx
@@ -3,11 +3,37 @@
 import React, { useEffect, useContext, useCallback, useState } from "react";
 import axios from "axios";
 import { BookContext } from "../utils/context/BookContext";
+import { useNavigate } from "react-router-dom";
 
 export default function AccountPage() {
-  const { userData, setUserData } = useContext(BookContext);
+  const navigate = useNavigate();
+  const {
+    userData,
+    setUserData,
+    setSuccessMessage,
+    setErrorMessage,
+    successMessage,
+    errorMessage,
+    setIsAuthenticatedNavbar,
+  } = useContext(BookContext);
   const [userDetails, setUserDetails] = useState({});
   const userId = localStorage.getItem("userId");
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedUserData, setEditedUserData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const handleSignOut = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("userId");
+    localStorage.removeItem("email");
+    localStorage.removeItem("password");
+    setUserData(null);
+    navigate("/SignIn");
+    setIsAuthenticatedNavbar(false);
+  };
 
   const fetchUserData = useCallback(async () => {
     try {
@@ -48,17 +74,115 @@ export default function AccountPage() {
     fetchUserData();
   }, [fetchUserData]);
 
+  const handleEditClick = () => {
+    setIsEditing(!isEditing);
+    setEditedUserData({ ...userDetails });
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setEditedUserData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleDoneEditing = async () => {
+    try {
+      const token = localStorage.getItem("token");
+      const email = localStorage.getItem("email");
+      const password = localStorage.getItem("password");
+
+      const headers = {
+        Authorization: `Bearer ${token}`,
+      };
+
+      const usernameChanged = editedUserData.username !== userDetails.username;
+      const emailChanged = editedUserData.email !== userDetails.email;
+      const passwordChanged = editedUserData.password !== userDetails.password;
+
+      if (!usernameChanged && !emailChanged && !passwordChanged) {
+        console.log("No changes made to user data");
+        setIsEditing(false);
+        setErrorMessage("No changes made to user data");
+        setTimeout(() => {
+          setErrorMessage("");
+        }, 3000);
+        // Show a message that no changes were made
+        return;
+      }
+
+      await axios.put(`http://localhost:3000/user/${userId}`, editedUserData, {
+        headers: headers,
+        auth: {
+          username: email,
+          password: password,
+        },
+      });
+
+      setIsEditing(false);
+      fetchUserData();
+      setSuccessMessage("Account details updated successfully");
+      setTimeout(() => {
+        setSuccessMessage("");
+      }, 3000);
+    } catch (error) {
+      console.log("Error updating user data");
+    }
+  };
+
   return (
     <div>
-      <h1>AccountPage</h1>
+      <h1 className="text-center mb-5 font-semibold">Account Details</h1>
       {userDetails && (
         <div>
-          <p>Username: {userDetails.username}</p>
-          <p>Email: {userDetails.email}</p>
+          <p>
+            Username:{" "}
+            {isEditing ? (
+              <input
+                type="text"
+                name="username"
+                value={editedUserData.username}
+                onChange={handleInputChange}
+              />
+            ) : (
+              userDetails.username
+            )}
+          </p>
+          <p>
+            Email:{" "}
+            {isEditing ? (
+              <input
+                type="email"
+                name="email"
+                value={editedUserData.email}
+                onChange={handleInputChange}
+              />
+            ) : (
+              userDetails.email
+            )}
+          </p>
           {/* Displaying the password in plain text is not recommended for security reasons */}
           <p>Password: {userDetails.password}</p>
         </div>
       )}
+      {successMessage ?? <p>{successMessage}</p>}
+      {errorMessage ?? <p>{errorMessage}</p>}
+      <button
+        className="flex m-auto bg-blue-500 text-white rounded-lg py-2 px-2 hover:scale-105"
+        onClick={isEditing ? handleDoneEditing : handleEditClick}
+      >
+        {isEditing ? "Done Updating 👍" : "Update ✍️"}
+      </button>
+      <button
+        onClick={handleSignOut}
+        className="flex m-auto bg-red-500 text-white rounded-lg py-2 px-2 hover:scale-105 mt-5"
+      >
+        Sign out ✌️
+      </button>
+      <button className="flex m-auto bg-red-500 text-white rounded-lg py-2 px-2 hover:scale-105 mt-5">
+        Delete Account 😭
+      </button>
     </div>
   );
 }

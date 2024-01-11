f37ae5efde88fc15914954843f3f431a215454a5
meghu <meghuraval1234@yahoo.com>
2024-01-10 20:35:30 -0500

    hopefullt the last step, i will try the deployed code now

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173/Addbook",
+    origin: "https://bookstore-mern-1hjk.onrender.com/books/Addbook",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     allowedHeaders: "Content-Type,Authorization",
   })
